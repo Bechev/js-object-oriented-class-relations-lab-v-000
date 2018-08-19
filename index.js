f78c6eb4c350fa9341bacc8486d387c3fc1ce814
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-19 15:55:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let store = {drivers: [], passengers: [], trips: []}
 
 let driverId = 0;
 let passengerId = 0;
+let tripId = 0;
 class Driver {
   constructor(name){
     this.name = name;
@@ -16,4 +17,8 @@ class Passenger{
     this.id = ++passengerId;
     store.passengers.push(this);
   }
+}
+
+class Trip{
+  constructor
 }
